4c4570908056909595ed86fa5d7de00c14862239
Marija <marija.kovacic.bg@gmail.com>
2018-06-06 12:43:48 +0200

    zadatak prva postavka

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+
+import student.Student;
+
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+        
+        Student student1 = new Student();
+        student1.setFirstName("Marija");
+        student1.setLastName("Kovacic");
+        student1.setYearOfBirth(1982);
+        student1.getCourse().setName("QA");
+        student1.getCourse().setNumberOfClasses(16);
+        student1.getComputer().setProcessTact(2.4);
+        student1.getComputer().setMemory(16.0);
+        student1.getComputer().setHardDrive(500);
+        student1.info();
+    }
+    
+}
---
+++ src/student/Student.java
@@ -0,0 +1,87 @@
+
+package student;
+
+import computer.Computer;
+import course.Course;
+
+
+public class Student {
+    
+    private String firstName;
+    private String lastName;
+    private int yearOfBirth;
+    
+    private Course course;
+    private Computer computer;
+    
+    
+    public Student() {
+        this.course = new Course();
+        this.computer = new Computer();
+    }
+    
+    
+    
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    
+    
+    // gett sett za klase
+    
+    
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public Computer getComputer() {
+        return computer;
+    }
+
+    public void setComputer(Computer computer) {
+        this.computer = computer;
+    }
+    
+    
+    
+    
+    
+    public void info () {
+        System.out.println("First name: " +getFirstName());
+        System.out.println("Last name: " +getLastName());
+        System.out.println("Year of birth: " +getYearOfBirth());
+        System.out.println("Course name: " +getCourse().getName());
+        System.out.println("Nomber of classes" +getCourse().getNumberOfClasses());
+        System.out.println("Hard drive: " +getComputer().getHardDrive());
+        System.out.println("Memory: " +getComputer().getMemory());
+        System.out.println("Process Tact: "+getComputer().getProcessTact());
+        
+    }
+    
+}
